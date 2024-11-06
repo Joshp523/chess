0767f80127538b7401247f43283f710e5fa61b5c
Joshua Riley <joshp523@byu.edu>
2024-11-06 16:46:15 -0700

    implemented and passed more tests

--- server/src/test/java/service/dataaccess/SqlAuthTest.java
+++ server/src/test/java/service/dataaccess/SqlAuthTest.java
@@ -1,10 +1,14 @@
 package service.dataaccess;
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.sql.SqlAuth;
+import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,13 +17,14 @@ class SqlAuthTest {
 
     @BeforeEach
     void setUp() {
-        UserData ud = new UserData("dummy", "password", "dummy@byu.edu");
         assertDoesNotThrow(() -> {sqlauth = new SqlAuth();});
+        UserData ud = new UserData("dummy", "password", "dummy@byu.edu");
         assertDoesNotThrow(() -> {sqlauth.createAuthToken(ud);});
     }
 
     @AfterEach
     void tearDown() {
+        assertDoesNotThrow(() -> {sqlauth.clearTokens();});
     }
 
     @Test
@@ -29,22 +34,63 @@ class SqlAuthTest {
 
     @Test
     void posCreateAuthToken() {
+        UserData moreData = new UserData("dumb", "passwordisme", "dumb@byu.edu");
+        assertDoesNotThrow(() -> {sqlauth.createAuthToken(moreData);});
     }
 
     @Test
     void negCreateAuthToken() {
+        UserData ud = new UserData(null, "password", "dummy@byu.edu");
+        assertThrows(DataAccessException.class, () -> {sqlauth.createAuthToken(ud);});
     }
 
     @Test
     void posDeleteAuthToken() {
+        UserData moreUserData = new UserData("dumb", "passwordisme", "dumb@byu.edu");
+        String token = null;
+        try {
+            token = sqlauth.createAuthToken(moreUserData);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        String finalToken = token;
+        AuthData testAuthData = new AuthData(finalToken,moreUserData.username());
+        assertDoesNotThrow(() -> {sqlauth.deleteAuthToken(testAuthData);});
     }
 
     @Test
     void negDeleteAuthToken() {
+        UserData moreUserData = new UserData("dumb", "passwordisme", "dumb@byu.edu");
+        String token = null;
+        try {
+            sqlauth.createAuthToken(moreUserData);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        AuthData testAuthData = new AuthData("badToken",moreUserData.username());
+        assertDoesNotThrow(() -> {sqlauth.deleteAuthToken(testAuthData);});
     }
 
     @Test
     void posFindByToken() {
+        UserData moreUserData = new UserData("dumb", "passwordisme", "dumb@byu.edu");
+        String token = null;
+        try {
+            token = sqlauth.createAuthToken(moreUserData);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        String finalToken = token;
+        AuthData expectedAuthData = new AuthData(finalToken, moreUserData.username());
+        AuthData actualAuthData;
+        try {
+            actualAuthData = sqlauth.findByToken(finalToken);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(expectedAuthData, actualAuthData);
     }
 
     @Test
