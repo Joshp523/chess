df41bc19a36977c9e766185cd79542c113c94cb0
Joshua Riley <joshp523@byu.edu>
2024-11-06 22:30:47 -0700

    done

--- server/src/test/java/service/dataaccess/SqlGameTest.java
+++ server/src/test/java/service/dataaccess/SqlGameTest.java
@@ -1,5 +1,6 @@
 package service.dataaccess;
 
+import chess.ChessGame;
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
@@ -11,6 +12,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static chess.ChessGame.TeamColor.BLACK;
+import static chess.ChessGame.TeamColor.WHITE;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SqlGameTest {
@@ -43,36 +46,79 @@ class SqlGameTest {
 
     @Test
     void posCreateGame() {
-        assertDoesNotThrow(() -> {sqlgame.createGame("cool new game");});
+        assertDoesNotThrow(() -> {
+            sqlgame.createGame("cool new game");
+        });
     }
 
     @Test
     void negCreateGame() {
-        assertDoesNotThrow(() -> {sqlgame.createGame("cool new game");});
-        assertThrows(DataAccessException.class, () -> {sqlgame.createGame("cool new game");});
+        assertDoesNotThrow(() -> {
+            sqlgame.createGame("cool new game");
+        });
+        assertThrows(DataAccessException.class, () -> {
+            sqlgame.createGame("cool new game");
+        });
     }
 
     @Test
     void posGetAllGames() {
-        assertDoesNotThrow(() -> {sqlgame.createGame("cool new game");});
-        assertDoesNotThrow(() -> {sqlgame.createGame("kinda cool game");});
-        assertDoesNotThrow(() -> {sqlgame.createGame("silly game");});
+        assertDoesNotThrow(() -> {
+            sqlgame.createGame("cool new game");
+        });
+        assertDoesNotThrow(() -> {
+            sqlgame.createGame("kinda cool game");
+        });
+        assertDoesNotThrow(() -> {
+            sqlgame.createGame("silly game");
+        });
         assertEquals(3, sqlgame.getAllGames().size());
     }
 
     @Test
     void negGetAllGames() {
-        assertDoesNotThrow(() -> {sqlgame.createGame("cool new game");});
-        assertDoesNotThrow(() -> {sqlgame.createGame("kinda cool game");});
-        assertDoesNotThrow(() -> {sqlgame.createGame("silly game");});
+        assertDoesNotThrow(() -> {
+            sqlgame.createGame("cool new game");
+        });
+        assertDoesNotThrow(() -> {
+            sqlgame.createGame("kinda cool game");
+        });
+        assertDoesNotThrow(() -> {
+            sqlgame.createGame("silly game");
+        });
         assertNotEquals(0, sqlgame.getAllGames().size());
     }
 
     @Test
     void posAddUser() {
+        int ied;
+        try {
+            ied = sqlgame.createGame("cool new game");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertDoesNotThrow(() -> {
+            sqlgame.addUser("dumb", WHITE, ied);
+        });
+        assertDoesNotThrow(() -> {
+            sqlgame.addUser("dumber", BLACK, ied);
+        });
     }
 
     @Test
     void negAddUser() {
+        int ied;
+        try {
+            ied = sqlgame.createGame("cool new game");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertDoesNotThrow(() -> {
+            sqlgame.addUser("dumb", WHITE, ied);
+        });
+        assertThrows(DataAccessException.class, () -> {
+            sqlgame.addUser("dumber", WHITE, ied);
+        });
+
     }
 }
