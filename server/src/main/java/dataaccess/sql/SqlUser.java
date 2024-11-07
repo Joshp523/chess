ed877c756c64ca1153c5f201f827b66e7e169cec
Joshua Riley <joshp523@byu.edu>
2024-11-07 12:09:58 -0700

    capitalization error fixed

--- server/src/main/java/dataaccess/sql/SqlUser.java
+++ server/src/main/java/dataaccess/sql/SqlUser.java
@@ -23,7 +23,7 @@ public class SqlUser implements UserDAO {
 
     private final String[] createStatements = {
             """
-            CREATE TABLE IF NOT EXISTS  Usertable (
+            CREATE TABLE IF NOT EXISTS  usertable (
               `username` varchar(256) NOT NULL,
               `password` varchar(256) NOT NULL,
               `email` varchar(256) NOT NULL,
