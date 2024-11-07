a3b7b5480c21bc8fbaf243bc27e9e55b8fbc34f5
Joshua Riley <joshp523@byu.edu>
2024-11-06 17:50:05 -0700

    wrote more tests

--- server/src/test/java/service/dataaccess/SqlUserTest.java
+++ server/src/test/java/service/dataaccess/SqlUserTest.java
@@ -1,9 +1,32 @@
 package service.dataaccess;
 
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+import dataaccess.sql.SqlAuth;
+import dataaccess.sql.SqlUser;
+import model.UserData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 class SqlUserTest {
+    UserDAO sqluser;
+
+    @BeforeEach
+    void setUp() {
+        assertDoesNotThrow(() -> {
+            sqluser = new SqlUser();
+        });
+        UserData ud = new UserData("dummy", "password", "dummy@byu.edu");
+    }
+
+    @AfterEach
+    void tearDown() {
+        assertDoesNotThrow(() -> {
+            sqluser.clearUsers();
+        });
+    }
 
     @Test
     void clearUsers() {
