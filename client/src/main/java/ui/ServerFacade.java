cf6b9724867858e45b94baeb8a6d305ecb0d67d7
Joshua Riley <joshp523@byu.edu>
2024-11-14 00:34:31 -0700

    made a skeleton of my serverFacade

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,5 +1,15 @@
 package ui;
+import chess.ChessGame;
 import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 
 public class ServerFacade {
@@ -9,7 +19,72 @@ public class ServerFacade {
         serverUrl = url;
     }
 
-    public void
+    public ChessGame[] listGames() {
+        var path = "/game";
+        record listGamesResponse(ChessGame[] games) {
+        }
+        var response = this.makeRequest("GET", path, null, listGamesResponse.class);
+        return response.games();
+    }
+    public String join(){
+        var path = "/game";
+        return this.makeRequest("PUT", path, null, String.class);
+    }
+    public Object createGame(String gameName){
+        var path = "/game";
+        return this.makeRequest("POST", path, null, String.class);
+    }
+    public String logout(){
+        var path = "/session";
+        return this.makeRequest("DELETE", path, null, String.class);
+    }
+    public String login(){
+        var path = "/session";
+        return this.makeRequest("GAME", path, null, String.class);
+    }
+
+    public String register(){
+        var path = "/user";
+        return this.makeRequest("POST", path, null, String.class);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+            writeBody(request, http);
+            http.connect();
+            return readBody(http, responseClass);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
 }
